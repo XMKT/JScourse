b1194ace1c04df4315d3234810486b975f51601f
XMKT <bezrukov005@gmail.com>
2026-01-28 17:15:27 +0300

    8. Работа с объектами: создание, изменение, удаление свойств

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ let userAge = 25; // let — можно изменить позже
 userAge = 26; // изменяем значение
 
 console.log(userName, userAge);
+
+const str = "текст"; // string
+const num = 42; // number
+const bool = true; // boolean
+const empty = null; // null
+const notDefined = undefined; // undefined
+const obj = { key: "value" }; // object (ссылочный тип)
+
+const city = {
+    name: "My city",
+    population: 8000000,
+};
+
+city.name = "Dzerzhinsk"; // изменение свойства
+city.country = "RUSSIA"; // добавление свойства
+delete city.population; // удаление свойства
+
+console.log(city);
