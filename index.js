2d4b7c2917d65f85415f3a7c607afde757137e69
XMKT <bezrukov005@gmail.com>
2026-01-28 17:22:13 +0300

    11. Работа с объектами: сокращённая запись свойств

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-console.log("Запуск кода в VS Code");
+let value = 10; // число
+value = "текст"; // теперь строка — ошибки не будет
+console.log(value); // 'текст'
 
-const user = {
-    name: "Андрей",
-    age: 30,
+const fixed = 100;
+// fixed = 200;     // ошибка: Assignment to constant variable
+
+const name = "Алексей";
+const age = 20;
+const userProfile = {
+    name, // вместо name: name
+    age, // вместо age: age
     isStudent: true,
 };
 
-console.log("Hello world"); // вывод строки
-console.dir(console); // просмотр свойств объекта console
-console.table({ a: 1, b: 2 }); // вывод в виде таблицы
-
-const userName = "Андрей"; // const — значение не изменится
-let userAge = 25; // let — можно изменить позже
-userAge = 26; // изменяем значение
-
-console.log(userName, userAge);
-
-const str = "текст"; // string
-const num = 42; // number
-const bool = true; // boolean
-const empty = null; // null
-const notDefined = undefined; // undefined
-const obj = { key: "value" }; // object (ссылочный тип)
-
-const city = {
-    name: "My city",
-    population: 8000000,
-};
-
-city.name = "Dzerzhinsk"; // изменение свойства
-city.country = "RUSSIA"; // добавление свойства
-delete city.population; // удаление свойства
-
-console.log(city);
+console.log(userProfile);
