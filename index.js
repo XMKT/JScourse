01b0dd8e88e8ed841c69e1b7656918b91b53b6d0
XMKT <bezrukov005@gmail.com>
2026-02-12 03:59:30 +0300

    48. Метод unshift

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-function log(value) {
-    console.log(value);
-}
-log(2 + 3); // 5 (выражение)
-// log(let x = 5); // Ошибка: инструкция недопустима как аргумент
-
-const arr1 = [1, 2, 3];
-const arr2 = new Array(1, 2, 3);
-console.log(arr1[0]); // 1
-
-const arr = [1, 2, 3];
-arr.push(4);
-console.log(arr); // [1, 2, 3, 4]
-const removed = arr.pop();
-console.log(removed); // 4
+const arr = [2, 3];
+arr.unshift(1);
 console.log(arr); // [1, 2, 3]
+const first = arr.shift();
+console.log(first); // 1
+console.log(arr); // [2, 3]
