614cd645bc72c1bf88ed169a539e79c6dfe773db
XMKT <bezrukov005@gmail.com>
2026-02-12 03:16:59 +0300

    34. Функции можно объявлять как Function Declaration

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-console.log(Boolean("")); // false
-console.log(!!0); // false
-console.log(!!"Hello"); // true
+console.log(10 + " apples"); // "10 apples"
 
-let value;
-console.log(typeof value === "undefined"); // true
-console.log(typeof 10 === "number"); // true
+function sum(a, b) {
+    return a + b;
+}
 
-console.log(!10); // false
-console.log(!!0); // false
-console.log(!!"text"); // true
-
-console.log("Hello" && "World"); // "World"
-console.log("" || "Default"); // "Default"
-
-let userName = someInput || "Guest";
-
-let isLogged = true;
-isLogged && console.log("User is logged in");
-
-const button = { id: 1, text: "Click" };
-const redButton = { ...button, color: "red" };
-console.log(redButton); // { id: 1, text: "Click", color: "red" }
-
-const obj1 = { x: 1, y: 2 };
-const obj2 = { y: 3, z: 4 };
-const merged = { ...obj1, ...obj2 }; // { x: 1, y: 3, z: 4 }
-
-let name = "John";
-let greeting = `Hello, ${name}!`;
-console.log(greeting); // Hello, John!
+// Function Expression
+const multiply = function (a, b) {
+    return a * b;
+};
