07e8d85b0b7e3c0c94913efc7c2e387d27c23ef0
XMKT <bezrukov005@gmail.com>
2026-02-12 04:48:48 +0300

    66. Цикл `for...in`

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-let val = 11;
-const result = val >= 0 ? val : -val;
-console.log(result); // 11
-
-// Тернарный оператор в вызове функции
-console.log(val >= 0 ? val : -val); // 11
-
-for (let i = 0; i < 5; i++) {
-    console.log(i); // 0, 1, 2, 3, 4
+const myObject = { x: 10, y: true, z: "bc" };
+for (const key in myObject) {
+    console.log(`${key}: ${myObject[key]}`);
 }
-
-const arr = ["frost", "second", "third"];
-arr.forEach((element, index) => console.log(`${index}: ${element}`));
-
-let i = 0;
-while (i < 5) {
-    console.log(i); // 0, 1, 2, 3, 4
-    i++;
-}
-
-i = 0;
-do {
-    console.log(i); // 0
-    i++;
-} while (i < 0);
