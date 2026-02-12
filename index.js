dc9fba0b4b5e3e0997f8d846277de0b869957dc1
XMKT <bezrukov005@gmail.com>
2026-02-12 04:04:53 +0300

    59. Использование If для проверки аргументов

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-let value = 10;
-if (value > 5) {
-    value += 20;
+function sumPositiveNums(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "One of the arguments is not a number";
+    }
+    if (a <= 0 || b <= 0) {
+        return "Numbers are not positive";
+    }
+    return a + b;
 }
-console.log(value); // 30
-
-let age = 25;
-if (age > 18) {
-    console.log("Adult");
-}
-if (age >= 12 && age <= 18) {
-    console.log("Teenager");
-}
-if (age < 12) {
-    console.log("Child");
-}
+console.log(sumPositiveNums(3, 8)); // 11
