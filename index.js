cad66494c49bfab48c33a0fa5e487e384e757b85
XMKT <bezrukov005@gmail.com>
2026-02-12 04:46:12 +0300

    65. Цикл `do...while`

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-let month = 2;
-switch (month) {
-    case 12:
-    case 1:
-    case 2:
-        console.log("Winter");
-        break;
-    default:
-        console.log("Not a winter month");
+let val = 11;
+const result = val >= 0 ? val : -val;
+console.log(result); // 11
+
+// Тернарный оператор в вызове функции
+console.log(val >= 0 ? val : -val); // 11
+
+for (let i = 0; i < 5; i++) {
+    console.log(i); // 0, 1, 2, 3, 4
 }
+
+const arr = ["frost", "second", "third"];
+arr.forEach((element, index) => console.log(`${index}: ${element}`));
+
+let i = 0;
+while (i < 5) {
+    console.log(i); // 0, 1, 2, 3, 4
+    i++;
+}
+
+i = 0;
+do {
+    console.log(i); // 0
+    i++;
+} while (i < 0);
