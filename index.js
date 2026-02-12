e86377c23fa296fff73a292ab64c9c8e259e44b7
XMKT <bezrukov005@gmail.com>
2026-02-12 03:58:51 +0300

    47. Метод `push`

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-try {
-    throw new Error("Something went wrong");
-} catch (error) {
-    console.error(error.message); // Something went wrong
+function log(value) {
+    console.log(value);
 }
-console.log("Code continues"); // Выполнится после обработки ошибки
+log(2 + 3); // 5 (выражение)
+// log(let x = 5); // Ошибка: инструкция недопустима как аргумент
+
+const arr1 = [1, 2, 3];
+const arr2 = new Array(1, 2, 3);
+console.log(arr1[0]); // 1
+
+const arr = [1, 2, 3];
+arr.push(4);
+console.log(arr); // [1, 2, 3, 4]
+const removed = arr.pop();
+console.log(removed); // 4
+console.log(arr); // [1, 2, 3]
